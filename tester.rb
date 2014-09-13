877ce9ff0378680a2ba212c25f9c7a5b39135fcb
Vadim Evard <v.e.evard@gmail.com>
2014-09-13 16:56:52 +0400

    REPL, selective test run and compiler invocation

--- tester.rb
+++ tester.rb
@@ -1,30 +1,41 @@
 require 'yaml'
 require 'processpilot/processpilot'
-progs = YAML.load_file "test.yaml"
-progs.each do |prog|
-  puts "running #{prog}..."
-  pn = prog["program"]
-  tc = 0
-  prog["tests"].each do |i, o|
-    tc += 1
-    puts "Test #{tc}:\n" #{i}\n=>\n#{o}\n--\n"
-    ProcessPilot::pilot(pn) do |cin, cout, cerr, cp|
-      
-      cin.write i
-      cin.flush
-      
-      a = cout.read(:time_out_secs => 5)
-      if a != o
-        puts "ERROR: wrong output on test #{tc}"
-        puts "Input: #{i.inspect}"
-        puts "Expected: #{o.inspect}"
-        puts "Actual:   #{a.inspect}"
-        puts
-      else
-        puts "ok"
+puts "type 'all' to run all tests, 'exit' to exit, or 'program123' to run tests on program123"
+while true
+  print "> "
+  ans = gets.chop
+  exit if ans == "exit"
+  run_all = (ans == "all")
+
+  progs = YAML.load_file "test.yaml"
+  progs.each do |prog|
+    pn = prog["program"]
+    # TODO: make compiler run configurable
+    `#{File.dirname(__FILE__) + "/FPC2.6.4/bin/i386-win32/fpc.exe"} #{pn.sub ".exe", ".pas"}`
+    next if !run_all && pn != ans
+    puts "running #{prog}..."
+    tc = 0
+    prog["tests"].each do |i, o|
+      tc += 1
+      puts "Test #{tc}:\n" #{i}\n=>\n#{o}\n--\n"
+      ProcessPilot::pilot(pn) do |cin, cout, cerr, cp|
+        
+        cin.write i
+        cin.flush
+        
+        a = cout.read(:time_out_secs => 5)
+        if a != o
+          puts "ERROR: wrong output on test #{tc}"
+          puts "Input: #{i.inspect}"
+          puts "Expected: #{o.inspect}"
+          puts "Actual:   #{a.inspect}"
+          puts
+        else
+          puts "ok"
+        end
+
       end
 
     end
-
   end
 end
